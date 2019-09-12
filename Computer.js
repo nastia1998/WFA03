d381e03c08c80e19f1f82bebed707745e7e128e2
Анастасия Лещенко <leshencko.nastia2013@gmail.com>
2019-09-12 15:30:29 +0300

    added ultrabook and computing server

--- Computer.js
+++ Computer.js
@@ -1,34 +1,42 @@
-var Computer = function (_manufacturer, _processor) {
-    this.manufacturer = manufacturer;
-    this.processor = _processor;
-    this.hardDiskSize = 256;
-    this.numOfProcessorCores = 2;
+var Computer = function(manufacturer, processor) {
+  this.manufacturer = manufacturer;
+  this.processor = processor;
 
-    this.getManufacturer = function() {
-        return this.manufacturer;
-    }
-    this.setManufacturer = function(_newManufacturer) {
-        this.manufacturer = _newManufacturer;
-    }
+  this.getManufacturer = function() {
+    return this.manufacturer;
+  };
+  this.setManufacturer = function(newManufacturer) {
+    this.manufacturer = newManufacturer;
+  };
 
-    this.processor = function() {
-        return this.processor;
-    }
-    this.setProcessor = function(_newProcessor) {
-        this.processor = _newProcessor;
-    }
+  this.processor = function() {
+    return this.processor;
+  };
+  this.setProcessor = function(newProcessor) {
+    this.processor = newProcessor;
+  };
+};
 
-    this.getHardDiskSize = function() {
-        return this.hardDiskSize;
-    }
-    this.setHardDiskSize = function(_newHardDiskSize) {
-        this.hardDiskSize = _newHardDiskSize;
-    }
+var Ultrabook = function(manufacturer, processor, hardDiskSize) {
+  Computer.apply(this, arguments);
+  this.hardDiskSize = hardDiskSize;
 
-    this.getNumOfProcessorCores = function() {
-        return this.numOfProcessorCores;
-    }
-    this.setNumOfProcessorCores = function(_newNumOfProcessorCores) {
-        this.numOfProcessorCores = _newNumOfProcessorCores;
-    }
-}
+  this.getHardDiskSize = function() {
+    return this.hardDiskSize;
+  };
+  this.setHardDiskSize = function(newHardDiskSize) {
+    this.hardDiskSize = newHardDiskSize;
+  };
+};
+
+var ComputingServer = function(manufacturer, processor, numOfProcessorCores) {
+  Computer.apply(this, arguments);
+  this.numOfProcessorCores = numOfProcessorCores;
+
+  this.getNumOfProcessorCores = function() {
+    return this.numOfProcessorCores;
+  };
+  this.setNumOfProcessorCores = function(newNumOfProcessorCores) {
+    this.numOfProcessorCores = newNumOfProcessorCores;
+  };
+};
